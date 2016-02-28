6b19595503d20eed1251913228e18083dee4f454
Nolan Lawson <nolan.lawson@gmail.com>
2016-02-28 13:00:43 -0500

    add arraybuffer test

---
+++ arraybuffer.js
@@ -0,0 +1,25 @@
+function arrayBufferToBinaryString(buffer) {
+  var binary = "";
+  var bytes = new Uint8Array(buffer);
+  var length = bytes.byteLength;
+  for (var i = 0; i < length; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return binary;
+}
+
+function binaryStringToArrayBuffer(bin) {
+  var length = bin.length;
+  var buf = new ArrayBuffer(length);
+  var arr = new Uint8Array(buf);
+  for (var i = 0; i < length; i++) {
+    arr[i] = bin.charCodeAt(i);
+  }
+  return buf;
+}
+
+self.addEventListener('message', function (e) {
+  var message = JSON.parse(arrayBufferToBinaryString(e.data));
+  var buff = binaryStringToArrayBuffer(JSON.stringify(message));
+  self.postMessage(buff, [buff]);
+});
